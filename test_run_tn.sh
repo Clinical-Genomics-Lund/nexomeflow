29f1abbd938c442cc17f5c36682903b327314de4
ViktorHy <viktor_henmyr@hotmail.com>
2019-05-13 14:40:11 +0200

    small run changes

--- test_run_tn.sh
+++ test_run_tn.sh
@@ -1,5 +1,8 @@
-/data/bnf/sw/nextflow/nextflow run main_tn.nf \
-			       -params-file params.json \
-			       -with-singularity container_2019-04-03.sif \
-			       -with-dag test.dag.png
+/data/bnf/sw/nextflow/nextflow run exome.nf \
+			       --csv /trannel/proj/cmd-pipe/test_real_trio.csv \
+				   --outdir /trannel/proj/cmd-pipe/test-files/run_singularity \
+			       -with-singularity container_2019-05-13.sif \
+			       -with-dag test.dag.png \
+				   -resume
+
  
