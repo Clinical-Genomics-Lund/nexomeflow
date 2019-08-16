a54984bfa9716a6804e124a7527e6bf6cd408423
Viktor Henmyr <viktor_henmyr@hotmail.com>
2019-08-16 13:24:26 +0200

    .

--- test_run_exome.sh
+++ test_run_exome.sh
@@ -1,8 +1,8 @@
 /data/bnf/sw/nextflow/nextflow run exome.nf \
-			        --csv /trannel/proj/cmd-pipe/test_real_trio.csv \
-				--outdir /trannel/proj/cmd-pipe/test-files/run_singularity \
-				-with-singularity container_2019-05-14.sif \
-			      	-with-dag test.dag.png \
+			    --csv /trannel/proj/cmd-pipe/test_real_trio.csv \
+				--outdir /data/bnf/dev/viktor/NF_output \
+				-with-singularity container_bwamem2.sif \
+			    -with-dag test.dag.png \
 				-with-timeline \
 				-resume
 
